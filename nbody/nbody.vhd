be429be6fbb9f423979e62ee34a82f07134e4e1a
Ovidiu Panait <ovpanait@gmail.com>
2017-12-31 00:24:27 +0200

    Transform integer operations to floating point
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- nbody/nbody.vhd
+++ nbody/nbody.vhd
@@ -13,30 +13,26 @@ entity nbody is
 end nbody;
 		
 architecture arch of nbody is
+signal diff_x:	   unsigned(63 downto 0);
+signal diff_y:	   unsigned(63 downto 0);
 signal en_out1:	std_logic;
-signal diff_x:	   signed(10 downto 0);
-signal diff_y:	   signed(10 downto 0);
 
-signal r:			unsigned(20 downto 0);
+signal diff_x_sq:	unsigned(63 downto 0);
+signal diff_y_sq:	unsigned(63 downto 0);
 signal en_out2:	std_logic;
 
-signal r_3:			unsigned(63 downto 0);
+signal r:			unsigned(63 downto 0);
 signal en_out3:	std_logic;
 
-signal r_3_fp:		unsigned(63 downto 0);
+signal r_sq:		unsigned(63 downto 0);
+signal r_buf:		unsigned(63 downto 0);
 signal en_out4:	std_logic;
 
-signal mul_res:	unsigned(63 downto 0);
+signal r_cube:		unsigned(63 downto 0);
 signal en_out5:	std_logic;
 
-signal uns_out:	unsigned(63 downto 0);
 signal en_out6:	std_logic;
 
-signal add_out:	unsigned(63 downto 0);
-signal test_sig2: unsigned(63 downto 0) := "0011111111111000000000000000000000000000000000000000000000000000"; -- 1.5F
-signal test_sig1:	unsigned(63 downto 0) := "1011111100101010001101101110001011101011000111000100001100101101"; -- 123
-signal test_sig3: unsigned(63 downto 0) := "0011111111100000000000000000000000000000000000000000000000000000"; -- 0.5
-signal test_sig4:	unsigned(63 downto 0) := "0011111110100011100001110101000011000111101101010011011110101001"; 
 signal en_out7:	std_logic;
 
 -- fast inv square root - stage 1
@@ -61,31 +57,39 @@ signal fisr_res:	unsigned(63 downto 0);
 signal tmp_dbg:	unsigned(7 downto 0);
 begin
 
-	-- 1 stage - calculate 
-	pipe_stage1: work.pipe_st1
-		port map(clk => clk, reset => reset, en_in => '1', rx_a => "00000000011", rx_b => "00000000000", 
-		ry_a => "00000000000", ry_b => "00000000000", diff_x => diff_x, diff_y => diff_y, en_out=> en_out1);
+	-- 5 stage pipeline to calculate ((rx_a - rx_b)**2 + (ry_a -ry_b)**2)**3
+	-- calculate (rx_a - rx_b) and (ry_a - ry_b)
+	r_stage1: work.r_st1
+		port map(clk => clk, reset => reset, en_in => '1', 
+		rx_a => "0100000001110001001000000000000000000000000000000000000000000000", -- 274 
+		rx_b => "0100000010000001010110000000000000000000000000000000000000000000", -- 555 => 555 - 274 = 281
+		ry_a => "0000000000000000000000000000000000000000000000000000000000000000", -- 0
+		ry_b => "0100000001110011101100000000000000000000000000000000000000000000", -- 315
+		diff_x => diff_x, diff_y => diff_y, en_out=> en_out1);
 	
-	-- 1 stage - calculate r
-	pipe_stage2: work.pipe_st2
-		port map(clk => clk, reset => reset, en_in => en_out1, diff_x => diff_x, diff_y => diff_y, r => r,
-		en_out  => en_out2); 
+	-- calculate diffx ** 2 + diffy ** 2
+	r_stage2: work.r_st2
+		port map(clk => clk, reset => reset, en_in => en_out1, diff_x => diff_x, diff_y => diff_y, 
+		diff_x_sq => diff_x_sq, diff_y_sq => diff_y_sq, en_out  => en_out2); 
 	
-	-- 1-stage - calculate r^3
-	r_cube: work.r_cube
-		port map(clk => clk, reset => reset, en_in => en_out2, r_in => r, r_3 => r_3, en_out => en_out3);
+	-- calculate r
+	r_stage3: work.r_st3
+		port map(clk => clk, reset => reset, en_in => en_out2, diff_x_sq => diff_x_sq, diff_y_sq => diff_y_sq,
+		r => r, en_out => en_out3);
 	
-	r_cube_fp: work.unsigned_to_fp
-		port map(clk => clk, reset => reset, en_in => en_out3, input => r_3, output => r_3_fp,
+	-- calculate r**2
+	r_stage4: work.r_st4
+		port map(clk => clk, reset => reset, en_in => en_out3, r => r, r_buf => r_buf, r_sq => r_sq,
 		en_out => en_out4);
 	
-	fpmu_test: work.fpmu
-		port map(clk => clk, reset => reset, en_in => en_out4, a => test_sig4, b => test_sig4, result => mul_res,
+	-- calculate r**3
+	r_stage5: work.r_st5
+		port map(clk => clk, reset => reset, en_in => en_out4, r => r, r_sq => r_sq, r_cube => r_cube,
 		en_out => en_out5);
 
 	-- 4-stage pipelined fst inverse squared root
 	fast_isq1: work.fast_isq_st1
-		port map(clk => clk, reset => reset, en_in => en_out5, input => r_3_fp, x => x, in_half => in_half,
+		port map(clk => clk, reset => reset, en_in => en_out5, input => r_cube, x => x, in_half => in_half,
 		en_out => en_out6);
 	
 	fast_isq2: work.fast_isq_st2
@@ -100,20 +104,14 @@ begin
 		port map(clk => clk, reset => reset, en_in => en_out8, x_1_5 => x_1_5b, x_sqMx_half_x => x_sqMx_half_x, 
 		result => fisr_res, en_out => en_out9);
 		
---	fp_add: work.fp_add
---		port map(clk => clk, reset => reset, en_in => en_out4, a => test_sig2, b => test_sig1, result => add_out, en_out => en_out5);
-		
---	fp_to_u64:	work.ftou
---		port map(clk => clk, reset => reset, en_in => en_out5, input => mul_res, output => uns_out, debug => tmp_dbg, en_out => en_out6);
-		
 --	debug <= std_logic_vector(fisr_res(63 downto 56));
 --	debug <= std_logic_vector(fisr_res(55 downto 48));
 --	debug <= std_logic_vector(fisr_res(47 downto 40));
---	debug <= std_logic_vector(fisr_res(39 downto 32));
+	debug <= std_logic_vector(fisr_res(39 downto 32));
 --	debug <= std_logic_vector(fisr_res(31 downto 24));
 --	debug <= std_logic_vector(fisr_res(23 downto 16));
 --	debug <= std_logic_vector(fisr_res(15 downto 8));
-	debug <= std_logic_vector(fisr_res(7 downto 0));
+--	debug <= std_logic_vector(fisr_res(7 downto 0));
 --	debug <= std_logic_vector(tmp_dbg);
 		
 end arch;
